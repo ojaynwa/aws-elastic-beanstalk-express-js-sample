d58a56345608dd9d10c5e93cd60eb4bcd2d8826f
ojaynwa <jona342002@yahoo.com>
2021-05-17 00:17:13 -0400

    Full pipeline test

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Nwachukwu AWS Lab!'));
+app.get('/', (req, res) => res.send('Nwachukwu Code Pipeline AWS LAB!'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
